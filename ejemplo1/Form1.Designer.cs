5c7e9a5b474c10551511c4a55a594873ff7f764d
Hamzaeljatib <hamzaheljatib@gmail.com>
2024-02-12 00:01:33 -0300

    subida de archivo de prueba

---
+++ ejemplo1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Ejemplo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSaludar = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Saludar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSaludar
+            // 
+            this.lblSaludar.AutoSize = true;
+            this.lblSaludar.Location = new System.Drawing.Point(210, 193);
+            this.lblSaludar.Name = "lblSaludar";
+            this.lblSaludar.Size = new System.Drawing.Size(0, 13);
+            this.lblSaludar.TabIndex = 1;
+            this.lblSaludar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(213, 55);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(112, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(153, 58);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(32, 19);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(509, 322);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblSaludar);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PrimeraApp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSaludar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ ejemplo1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // MODIFICADOR PARTIAL: ES UN MODIFICADOR QUE PERMITE LA POSIBILIDAD DE CREAR UNA CLASE DISTRIBUIDA EN DISTINTOS FICHEROS O EN DISTINTOS ARCHIVOS
+            InitializeComponent();
+
+            //EVENTO: ES UN METODO ASOCIADO A UN DETERMINADO CONTEXTO( HACER CLICK EN EL BOTON Y QUE ESO EJECUTE TAL METODO ES EL EVENTO, LA ACCION EN CONJUNTO DE TODO)
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //clase estatica
+            //MessageBox.Show("Hola Mundo");
+            string texto = txtNombre.Text;
+            lblSaludar.Text = "HOLA " + texto;  
+        } 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // MessageBox.Show("Te doy la bienvenida");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           //MessageBox.Show("Gracias por usar la app!");
+        }
+
+    }
+}
